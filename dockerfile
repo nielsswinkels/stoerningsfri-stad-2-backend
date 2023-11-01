4369c804d573a5d6a0777b66cf3eb8ac8a4b61f8
Niels <niels.sw@gmail.com>
2023-11-01 14:05:43 +0100

    first version

---
+++ dockerfile
@@ -0,0 +1,21 @@
+# Use an official Node.js runtime as the base image
+FROM node:16.20.0
+
+# Set the working directory in the container to /app
+WORKDIR /app
+
+# Copy package.json and package-lock.json into the directory /app in the container
+COPY package*.json ./
+
+# Install any needed packages specified in package.json
+RUN npm install
+
+# Bundle the app source inside the Docker image 
+# (i.e., copy everything from the current directory into /app in the container)
+COPY . .
+
+# Make port 8080 available to the world outside the container
+EXPOSE 8080
+
+# Run the app when the container launches
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const { Pool } = require('pg');
+const { createTunnel } = require('tunnel-ssh');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+const sshOptions = {
+    host: process.env.SSH_HOST,
+    port: process.env.SSH_PORT,
+    username: process.env.SSH_USERNAME,
+    password: process.env.SSH_PASSWORD
+};
+
+const dbConfig = {
+    host: 'localhost',
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+};
+
+async function mySimpleTunnel(sshOptions, port, autoClose = true){
+    let forwardOptions = {
+        srcAddr:'127.0.0.1',
+        srcPort:port,
+        dstAddr: process.env.DESTINATION_IP,
+        dstPort: process.env.DESTINATION_PORT
+    }
+
+    let tunnelOptions = {
+        autoClose:autoClose
+    }
+    
+    let serverOptions = {
+        port: port
+    }
+
+    return createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions);
+}
+
+let pool
+
+async function connectDatabase(query) {
+  await mySimpleTunnel(sshOptions, dbConfig.port);
+  pool = new Pool(dbConfig);
+}
+
+async function runQuery(query) {
+    return await pool.query(query);
+}
+
+app.get('/', async (req, res) => {
+    const result = await runQuery('SELECT * FROM sfs.dow LIMIT 100');
+    res.send(result.rows);
+});
+
+app.get('/dow', async (req, res) => {
+    const result = await runQuery('SELECT * FROM sfs.dow LIMIT 100');
+    res.send(result.rows);
+});
+
+app.get('/scenario', async (req, res) => {
+    const result = await runQuery('SELECT * FROM sfs.scenario LIMIT 100');
+    res.send(result.rows);
+});
+
+app.get('/sim_links', async (req, res) => {
+    const result = await runQuery('SELECT st_astext(st_transform(geom, 4326))  FROM sfs.sim_links LIMIT 100');
+    res.send(result.rows);
+});
+
+
+
+app.listen(port, () => {
+    connectDatabase();
+    console.log(`App listening at http://localhost:${port}`);
+});
